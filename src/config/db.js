28e72ed22cb8fa6f76728c0d3f640f19944ae31c
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-11-28 02:46:13 +0100

    Añadido debug logs para db

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,35 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+// --- CHIVATO DE DEBUG (Bórralo cuando funcione) ---
+console.log("-----------------------------------------");
+console.log("INTENTANDO CONECTAR A LA BASE DE DATOS...");
+console.log(
+  "HOST:",
+  process.env.DB_HOST ? process.env.DB_HOST : "¡ESTÁ VACÍO! USANDO LOCALHOST"
+);
+console.log(
+  "PORT:",
+  process.env.DB_PORT ? process.env.DB_PORT : "¡VACÍO! USANDO 3306"
+);
+console.log("USER:", process.env.DB_USER);
+console.log("DB:", process.env.DB_NAME);
+console.log("-----------------------------------------");
+// ---------------------------------------------------
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // Agregamos el puerto aquí por si acaso
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
     dialectOptions: {
       ssl: {
-        require: true, // Obligatorio para Aiven
-        rejectUnauthorized: false, // Evita errores de certificados en la capa gratuita
+        require: true,
+        rejectUnauthorized: false,
       },
     },
     pool: {
