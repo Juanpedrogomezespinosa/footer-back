ece2bef1ed22dd49087e5369cfdb84e5077c5d66
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-11-28 02:33:35 +0100

    Configuracion db para produccion SSL

--- src/config/db.js
+++ src/config/db.js
@@ -7,8 +7,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Agregamos el puerto aquí por si acaso
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // Obligatorio para Aiven
+        rejectUnauthorized: false, // Evita errores de certificados en la capa gratuita
+      },
+    },
     pool: {
       max: 10,
       min: 0,
